76ed6879288b5000a1f18a7b2b0c46b17d7bce30
danshaman05 <dany05@email.cz>
2020-02-26 23:39:28 +0100

    spojenie s MySQL DB

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:SuperBakalarka1.@localhost/candle_2016_2017_zima'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
