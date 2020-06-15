2e7055a44c855716dacf0f751f2e2750415c9f28
danshaman05 <dany05@email.cz>
2020-06-15 22:43:49 +0200

    Rozvrh pre daneho ucitela.

--- candle_backend/models.py
+++ candle_backend/models.py
@@ -3,32 +3,50 @@ from candle_backend import db
 # TODO Treba popridavat nullable=False, db.ForeignKey k atributom
 
 class Room(db.Model):
-    __tablename__ = 'room'
-    id = db.Column('id', db.BIGINT, primary_key=True)
-    name = db.Column('name', db.String(30))
-    room_type_id = db.Column("room_type_id", db.BIGINT)  # treba nastavit cudzi kluc?
-    capacity = db.Column("capacity", db.BIGINT)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    room_type_id = db.Column(db.Integer, db.ForeignKey('room_type.id'), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+
+    lessons = db.relationship('Lesson', backref='room', lazy='dynamic')     # dynamic preto, aby sme s lessons mohli pracovat ako s query (mohli spustit order_by,...)
 
     def __repr__(self):
         return "<Room %r>" % self.name
 
 
+teacher_lessons = db.Table('teacher_lessons',
+                           db.Column('id', db.Integer, primary_key=True),  #  je v tabulke zbytocne? (primarny kluc je predsa dvojica atributov nizsie)
+                           db.Column('teacher_id', db.Integer, db.ForeignKey('teacher.id')),
+                           db.Column('lesson_id', db.Integer, db.ForeignKey('lesson.id'))
+)
 
-class Lesson(db.Model):
-    __tablename__ = 'lesson'
-    id = db.Column('id', db.BIGINT, primary_key=True)
-    day = db.Column('day',db.BIGINT)
-    start = db.Column("start", db.BIGINT)
-    end = db.Column("end", db.BIGINT)
-    lesson_type_id = db.Column("lesson_type_id", db.BIGINT, db.ForeignKey('lesson_type.id'))  # treba nastavit cudzi kluc?
-    room_id = db.Column("room_id", db.BIGINT)
-    subject_id = db.Column("subject_id", db.BIGINT)
-    external_id = db.Column("external_id", db.BIGINT)
-    note = db.Column("note", db.VARCHAR)
+class Teacher(db.Model):
+    id = db.Column('id', db.Integer, primary_key=True)
+    given_name = db.Column(db.String(50), nullable=True)
+    family_name = db.Column(db.String(50), nullable=False)
+    iniciala = db.Column(db.String(50), nullable=True)
+    oddelenie = db.Column(db.String(), nullable=True)
+    katedra = db.Column(db.String(), nullable=True)
+    external_id = db.Column(db.String(), nullable=True)
+    login = db.Column(db.String(), nullable=True)
+    slug = db.Column(db.String(), nullable=True)
 
+    lessons = db.relationship('Lesson', secondary=teacher_lessons, backref=db.backref('teachers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
-        return f"Lesson(id:'{self.id}', room_id:'{self.room_id}' )"
+        return f"Teacher(id:'{self.id}', given_name:'{self.given_name}' )"
+
+
+class Lesson(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day = db.Column(db.Integer, nullable=False)
+    start = db.Column(db.Integer, nullable=False)
+    end = db.Column(db.Integer, nullable=False)
+    lesson_type_id = db.Column(db.Integer, db.ForeignKey('lesson_type.id'), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    external_id = db.Column(db.Integer, nullable=True)
+    note = db.Column(db.VARCHAR, nullable=True)
 
 
     def getDayAbbreviation(self) -> str:
@@ -37,18 +55,14 @@ class Lesson(db.Model):
         days = ['Po', 'Ut', 'St', 'Št', 'Pi']
         return days[self.day]
 
-
-    def getListOfTeachers(self):
-        '''Vrati zoznam ucitelov'''
-
-
+    def __repr__(self):
+        return f"Lesson(id:'{self.id}', room_id:'{self.room_id}' )"
 
 
 class LessonType(db.Model):
-    __tablename__ = 'lesson_type'
-    id = db.Column('id', db.BIGINT, primary_key=True)
-    name = db.Column("name", db.VARCHAR)
-    code = db.Column("code", db.VARCHAR)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    code = db.Column(db.String(1), nullable=False)
 
     def __repr__(self):
         return f"{self.name}"
@@ -56,40 +70,25 @@ class LessonType(db.Model):
 
 
 class Subject(db.Model):
-    __tablename__ = 'subject'
-    id = db.Column('id', db.BIGINT, primary_key=True)
-    name = db.Column("name", db.VARCHAR)
-    code = db.Column("code", db.VARCHAR)
-    short_code = db.Column("short_code", db.VARCHAR)
-    credit_value = db.Column('credit_value', db.BIGINT)
-    rozsah = db.Column('rozsah', db.VARCHAR)
-    external_id = db.Column('external_id', db.VARCHAR)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    code = db.Column(db.String(50), nullable=False)
+    short_code = db.Column(db.String(20), nullable=False)
+    credit_value = db.Column(db.Integer, nullable=False)
+    rozsah = db.Column(db.String(30), nullable=True)
+    external_id = db.Column(db.String(30), nullable=True)
+
+    lessons = db.relationship('Lesson', backref='subject', lazy=True)  # lazy=True znamena, ze sa lessons nacitaju iba pri volani
 
     def __repr__(self):
         return f"Subject(id:'{self.id}', name:'{self.name}' )"
 
 
 
-class Teacher(db.Model):
-    __tablename__ = 'teacher'
-    id = db.Column('id', db.BIGINT, primary_key=True)
-    given_name = db.Column("given_name", db.VARCHAR)
-    family_name = db.Column("family_name", db.VARCHAR)
-    iniciala = db.Column("iniciala", db.VARCHAR)
 
-    oddelenie = db.Column("oddelenie", db.VARCHAR)
-    katedra = db.Column("katedra", db.VARCHAR)
-    eternal_id = db.Column("external_id", db.VARCHAR)
-    login = db.Column("login", db.VARCHAR)
-    slug = db.Column("slug", db.VARCHAR)
+#### ZATIAL NEVYUZITE:
 
-    def __repr__(self):
-        return f"Teacher(id:'{self.id}', given_name:'{self.given_name}' )"
-
-
-
-class Teacher_lessons(db.Model):
-    __tablename__ = 'teacher_lessons'
-    id = db.Column('id', db.BIGINT, primary_key=True)
-    teacher_id = db.Column('teacher_id', db.BIGINT, db.ForeignKey('teacher.id'))
-    lesson_id = db.Column('lesson_id', db.BIGINT, db.ForeignKey('lesson.id'))
+class RoomType(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    code = db.Column(db.String(1), nullable=False)
--- candle_backend/routes.py
+++ candle_backend/routes.py
@@ -1,5 +1,7 @@
+from typing import List, Dict
+
 from flask import render_template
-from candle_backend.models import Room, Lesson, LessonType, Subject, Teacher, Teacher_lessons
+from candle_backend.models import Room, Lesson, LessonType, Subject, Teacher
 from candle_backend import app
 from candle_backend.helpers import getRoomsSortedByDashes_dict, getTeachersSortedByLastname_dict, minutes2time, shortName
 
@@ -11,50 +13,23 @@ def home(): # TODO
 
 #### MODUL ROOM ####
 
-# Vypise vsetky miestnosti (zoznam)
 @app.route('/miestnosti')
 def list_rooms():
+    # Vypise vsetky miestnosti (zoznam)
     rooms = Room.query.all()
     rooms_dict = getRoomsSortedByDashes_dict(rooms)  # ucebne su v jednom dictionary rozdelene podla prefixu
 
     return render_template('show_all_rooms.html', rooms_dict=rooms_dict)
 
 
-# zobrazi rozvrh pre danu miestnost:
 @app.route('/miestnosti/<room_name>')
 def roomTimetable(room_name):
-    # nacitame z DB vsetky hodiny, kt. maju room == room_name
-    room_id1 = Room.query.filter_by(name=room_name).first()
-    lessons_objects = Lesson.query.filter_by(room_id=room_id1.id)\
-        .order_by(Lesson.day, Lesson.start)
+    # Zobrazi rozvrh pre danu miestnost:
+    room = Room.query.filter_by(name=room_name).first()
+    lessons_objects = room.lessons.order_by(Lesson.day, Lesson.start)
+    lessons_list = getLessons_list(lessons_objects)
+    return render_template('timetable_room.html', room_name=room_name, lessons=lessons_list)
 
-    lessons = []
-    for lo in lessons_objects:
-        subject = Subject.query.filter_by(id=lo.subject_id).first()
-        query = Teacher_lessons.query.filter_by(lesson_id=lo.id)
-        teacher_lessons_ids = [x.teacher_id for x in query.all()]  # chceme iba idcka ucitelov, kt. ucia dany predmet
-
-        teachers = Teacher.query.filter(Teacher.id.in_(teacher_lessons_ids)).all()
-
-        lesson = {}     # Kazda lesson bude dictionary.
-        teachers_dict = {}      # Jednu lesson moze ucit viac ucitelov, preto si vytvorime dict ucitelov
-        for teacher in teachers:
-            teacher = Teacher.query.filter_by(id=teacher.id).first()
-            teacher_short = shortName(teacher.given_name, teacher.family_name)
-            teachers_dict[teacher.slug] = teacher_short
-
-        lesson['teachers_dict'] = teachers_dict
-        lesson['day'] = lo.getDayAbbreviation()
-        lesson['start'] = minutes2time(lo.start)
-        lesson['end'] = minutes2time(lo.end)
-        lesson['room'] = room_name
-        lesson['type'] = LessonType.query.filter_by(id=lo.lesson_type_id).first().name
-        lesson['code'] = subject.short_code
-        lesson['subject'] = subject.name
-        lesson['note'] = lo.note if lo.note is not None else ''
-        lessons.append(lesson)
-
-    return render_template('timetable_room.html', room_name=room_name, lessons=lessons)
 
 
 
@@ -63,7 +38,7 @@ def roomTimetable(room_name):
 # Vypise vsetkych ucitelov (zoznam)
 @app.route('/ucitelia')
 def list_teachers():
-    teachers = Teacher.query.order_by(Teacher.family_name).all()
+    teachers = Teacher.query.order_by(Teacher.family_name)
     teachers_dict = getTeachersSortedByLastname_dict(teachers)  # ucebne su v jednom dictionary rozdelene podla prefixu
 
     return render_template('show_all_teachers.html', teachers_dict=teachers_dict)
@@ -71,6 +46,41 @@ def list_teachers():
 
 @app.route('/ucitelia/<teacher_slug>')
 def teacherTimetable(teacher_slug):
-    '''Ziskame'''
+    ''' Zobrazi rozvrh daneho ucitela.'''
+    teacher = Teacher.query.filter_by(slug=teacher_slug).first()
+    teacher_name = teacher.given_name + " " + teacher.family_name
 
-    return ''   # TODO !!
+    lessons_objects = teacher.lessons.order_by(Lesson.day, Lesson.start).all()
+    lessons_list = getLessons_list(lessons_objects)
+
+    return render_template('timetable_teacher.html', teacher_name=teacher_name, lessons=lessons_list)
+
+
+def getLessons_list(lessons_objects) -> List:
+    lessons_list: List[Dict] = []
+    for lo in lessons_objects:
+        subject = lo.subject
+        teachers = lo.teachers.all()
+
+        lesson_dict: Dict[str, str] = {}
+        teachers_dict: Dict[str, str] = {}  # Jednu lesson moze ucit viac ucitelov, preto si pre kazdu lesson vytvorime dict ucitelov
+
+        if len(teachers) == 1 and teachers[0].given_name == '':     # napr. predmet "pisomky" ma takeho ucitela
+            lesson_dict['teachers_dict'] = None
+        else:
+            for teacher in teachers:
+                teacher_short = shortName(teacher.given_name, teacher.family_name)
+                teachers_dict[teacher.slug] = teacher_short
+
+        lesson_dict['teachers_dict'] = teachers_dict
+        lesson_dict['day'] = lo.getDayAbbreviation()
+        lesson_dict['start'] = minutes2time(lo.start)
+        lesson_dict['end'] = minutes2time(lo.end)
+        lesson_dict['room'] = lo.room.name
+        lesson_dict['type'] = LessonType.query.get(lo.lesson_type_id)
+        lesson_dict['code'] = subject.short_code
+        lesson_dict['subject'] = subject.name
+        lesson_dict['note'] = lo.note if lo.note is not None else ''
+
+        lessons_list.append(lesson_dict)
+    return lessons_list
