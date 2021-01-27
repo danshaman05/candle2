6ed0bef540447174ab95ab18df33e5f03c3ab763
danshaman05 <dany05@email.cz>
2021-01-27 22:43:59 +0100

    Duplikovanie rozvrhu (refaktoring).

--- candle_backend/timetable_manager/views.py
+++ candle_backend/timetable_manager/views.py
@@ -47,52 +47,39 @@ def duplicate_timetable():
     """
 
     url_list = rozvrh_url.split('/')
-
+    old_timetable = None
     if "ucitelia" in url_list:
         i = url_list.index("ucitelia")
         slug = url_list[i + 1]    # pozicia ucitelovho slugu v url
-        teacher = Teacher.query.filter_by(slug=slug).first_or_404()  # podla slugu najdeme ucitela
-        new_t = UserTimetable(name=teacher.short_name, user_id=current_user.id)
-        db.session.add(new_t)
-        for lesson in teacher.lessons:
-            new_t.lessons.append(lesson)
-        db.session.commit()
-        return jsonify({'next_url': url_for("timetable.user_timetable", id_=new_t.id_)})
+        old_timetable = Teacher.query.filter_by(slug=slug).first_or_404()  # podla slugu najdeme ucitela
+        new_t = UserTimetable(name=old_timetable.short_name, user_id=current_user.id)
 
-    if "miestnosti" in url_list:
+    elif "miestnosti" in url_list:
         i = url_list.index("miestnosti")
         name = url_list[i + 1]
-        room = Room.query.filter_by(name=name).first_or_404()
+        old_timetable = Room.query.filter_by(name=name).first_or_404()
         new_t = UserTimetable(name=name, user_id=current_user.id)
-        db.session.add(new_t)
-        for lesson in room.lessons:
-            new_t.lessons.append(lesson)
-        db.session.commit()
-        return jsonify({'next_url': url_for("timetable.user_timetable", id_=new_t.id_)})
 
-    if "kruzky" in url_list:
+    elif "kruzky" in url_list:
         i = url_list.index("kruzky")
         name = url_list[i + 1]
-        student_group = StudentGroup.query.filter_by(name=name).first_or_404()
+        old_timetable = StudentGroup.query.filter_by(name=name).first_or_404()
         new_t = UserTimetable(name=name, user_id=current_user.id)
-        db.session.add(new_t)
-        for lesson in student_group.lessons:
-            new_t.lessons.append(lesson)
-        db.session.commit()
-        return jsonify({'next_url': url_for("timetable.user_timetable", id_=new_t.id_)})
 
-
-    if "moj-rozvrh" in url_list:
+    elif "moj-rozvrh" in url_list:
         i = url_list.index("moj-rozvrh")
         id = url_list[i + 1]
-        print("ID: " + id)
-        user_timetable = UserTimetable.query.get(id)
-        new_t = UserTimetable(name=user_timetable.name, user_id=current_user.id)
-        db.session.add(new_t)
-        for lesson in user_timetable.lessons:
-            new_t.lessons.append(lesson)
-        db.session.commit()
-        return jsonify({'next_url': url_for("timetable.user_timetable", id_=new_t.id_)})
+        old_timetable = UserTimetable.query.get(id)
+        new_t = UserTimetable(name=old_timetable.name, user_id=current_user.id)
+
+    else:
+        raise Exception("BAD URL!")        # TODO nahradit Error page!
+
+    db.session.add(new_t)
+    for lesson in old_timetable.lessons:
+        new_t.lessons.append(lesson)
+    db.session.commit()
+    return jsonify({'next_url': url_for("timetable.user_timetable", id_=new_t.id_)})
 
 
 
