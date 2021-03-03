34d695b253d2fbb8275d9d0474393b4fbea81448
danshaman05 <danshaman05@gmail.com>
2021-03-03 21:22:07 +0000

    aplikacia bezi na serveri
    Manualny load .env suboru; Balicek pymysql pridany do requirements.

--- candle_backend/config.py
+++ candle_backend/config.py
@@ -1,12 +1,22 @@
 import os
 
+from dotenv import load_dotenv
+
+# nacitame .env subor s premennymi prostredia:
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '../.env'))
+
+
 class Config:
     DEBUG = True
     TESTING = False
     CSRF_ENABLED = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    if not SECRET_KEY:
+        raise ValueError("No SECRET_KEY set for Flask application")
 
     SERVER_PATH = ''
     MYSQL_DATABASE_CHARSET = 'utf8mb4'  # Kvoli collation - zatial nevyuzivame.
