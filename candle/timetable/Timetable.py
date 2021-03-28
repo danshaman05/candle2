061871e26c7ee651cc04b07cfed44dd684c54e54
Daniel Grohol <danshaman05@gmail.com>
2021-03-28 21:32:07 +0200

    New algorithm for timetable rendering.
    Timetable is no more a table. Lessons use CSS "position: absolute" property for positioning. We got rid of many lines of code.

--- candle/timetable/Timetable.py
+++ candle/timetable/Timetable.py
@@ -37,9 +37,7 @@ class Timetable:
         self.__init_times()
         self.__lessons = lessons_objects
         self.__init_layout()
-        lessons_sorted_by_days = self.__sort_lessons_by_days()  # usporiadame si hodiny podla dni v tyzdni
-        self.__set_layout(lessons_sorted_by_days)
-        self.__init_last_started_lessons_list()
+        self.__set_layout()
 
     def __init_times(self):
         """
@@ -48,27 +46,18 @@ class Timetable:
         self.__starting_times = []
         for minutes in range(self.__TIME_MIN, self.__TIME_MAX, 50):
             self.__starting_times.append(self.minutes_2_time(minutes))
-        print(len(self.__starting_times))
 
     def __init_layout(self):
-        """ Inicializuje 2d list, ktoreho prvky su slovniky (dni -> stlpce -> slovnik)."""
-
+        """ Initializes 2d list which contains OrderedDict-s (days -> columns -> OrderedDict)."""
         self.__layout = []
         for i in range(5):
-            # I use OrderedDict instead of normal dict, because I want to easily access last added element:
-            od = OrderedDict()
+            # I use OrderedDict instead of normal dict, because I want to easily access a last added element:
+            od = OrderedDict()  # TODO: Maybe we should go only with lists...
             self.__layout.append([od])
 
-    def __init_last_started_lessons_list(self):
-        # Initializes attribute __last_started_lessons
-        self.__lessons_in_progress = []
-        for di in range(len(self.__layout)):
-            self.__lessons_in_progress.append([])
-            for ci in range(len(self.__layout[di])):
-                self.__lessons_in_progress[di].append(None)
 
-    def __sort_lessons_by_days(self):
-        """Rozdelime hodiny podla dni v tyzdni. Kazdy den bude predstavovat zoznam hodin."""
+    def __get_lessons_sorted_by_days(self):
+        """Vrati hodiny rozdelene do dni. Rozdelime hodiny podla dni v tyzdni. Kazdy den bude predstavovat zoznam hodin."""
 
         if self.__lessons is None:
             raise Exception("Attribute __lessons cannot be None")
@@ -80,38 +69,39 @@ class Timetable:
             days[lesson.day].append(lesson)
         return days
 
-    def __set_layout(self, lessons_sorted_by_days):
-        """ Nastavi layout rozvrhu: vlozi lessons do _layout, tak, ze ich roztriedi podla dni do stlpcov. Kazdy
-        stlpec je OrderedDict. Algoritmus sa vzdy snazi pridavat hodiny do co "najviac laveho" stlpca. """
+    def __set_layout(self):
+        """ Nastavi atribut self.__layout. Vlozi lessons do , tak, ze pre kazdy den vytvori
+         pozadovany pocet stlpcov. Kazdy stlpec je OrderedDict. Algoritmus sa vzdy snazi pridavat hodiny
+         do co "najviac laveho" stlpca. """
+        lessons_sorted_by_days = self.__get_lessons_sorted_by_days()  # usporiadame si hodiny podla dni v tyzdni
 
         # Rozdelime hodiny do stlpcov:
         # pre kazdy den v tyzdni
-        for i, lessons in enumerate(lessons_sorted_by_days):
+        for day_index, lessons in enumerate(lessons_sorted_by_days):
             # pre kazdu hodinu v danom dni:
             for lesson in lessons:
                 added = False
                 column_index = 0
-                # pre kazdy stlpec v danom dni:
-                for column_i, column in enumerate(self.__layout[i]):
-                    # ak mozes, skus don dat hodinu
-                    placed_lesson = PlacedLesson(lesson, column_i)
-                    if self.__can_add_lesson(placed_lesson, column):
-                        column[lesson.start] = placed_lesson  # miesto lesson pridavame PlacedLesson
-                        added = True
+                while added == False:
+                    if column_index == 6:
+                        raise Exception("Cannot add more than 6 neighbour lessons in one day!") # TODO test it!
+                    # if we don't have enough columns, create a new one:
+                    if len(self.__layout[day_index]) - 1 <  column_index:
+                        new_dict = OrderedDict()
+                        self.__layout[day_index].append(new_dict)
+                    # try to add the lesson:
+                    if self.__can_add_lesson(lesson, self.__layout[day_index][column_index]):
+                        self.__layout[day_index][column_index][lesson.start] = PlacedLesson(lesson, column_index)
                         break
-                # ak neslo dat, vytvor novy stlpec a vloz ju don
-                if not added:
-                    new_dict = OrderedDict()
-                    new_dict[lesson.start] = PlacedLesson(lesson)    # miesto lesson pridavame PlacedLesson
-                    self.__layout[i].append(new_dict)
+                    column_index += 1
 
-    def __can_add_lesson(self, placed_lesson, column: OrderedDict):
+
+    def __can_add_lesson(self, lesson, column: OrderedDict) -> bool:
         ''' Vrati True, ak sa hodina da vlozit do stlpca (neprebieha momentalne ina hodina v danom stlpci).
         Pre spravnu funkcnost musi byt column zoradeny podla casu.'''
-
         if len(column) == 0:
             return True
-        if self.__get_last_added_lesson(column).get_end() < placed_lesson.get_start():
+        if self.__get_last_added_lesson(column).get_end() < int(lesson.start):
             return True
         return False
 
@@ -120,10 +110,6 @@ class Timetable:
         last_lesson_key = next(reversed(column))
         return column[last_lesson_key]
 
-
-    def __end_lesson(self, day_index, column_index, lesson_key):
-        # Zmaze hodinu z __layout, kedze uz skoncila.
-        del self.__layout[day_index][column_index][lesson_key]
 
 
 
@@ -140,46 +126,10 @@ class Timetable:
     def get_starting_times(self) -> List[str]:
         return self.__starting_times
 
-
-    def get_lesson(self, day_index, column_index, time):
-        """Vrati hodinu pre dany den, stlpec a cas."""
-        lesson = self.__layout[day_index][column_index][time]
-        if lesson is None:
-            raise Exception("Lesson with these parameters doesn't exists.")
-        return lesson
-
-    def start_lesson(self, day_index, column_index, time):
-        # zapise ju do __lessons_in_progress
-        self.__lessons_in_progress[day_index][column_index] = time
-
-    def lesson_in_progress(self, actual_time, day_index, column_index):
-        """Vrati True, ak v danom stlpci a danom case prebieha hodina."""
-        lesson_key = self.get_lesson_key(day_index, column_index)
-        if lesson_key is None:
-            return False
-        lesson = self.get_lesson(day_index, column_index, lesson_key)
-
-        if lesson is None:
-            raise Exception("Lesson cannot be None.")
-
-        if (lesson.start < actual_time) and (lesson.end + lesson.get_breaktime() > actual_time):
-            return True
-        return False
-
-    def get_lesson_key(self, day_index, column_index):
-        """Vrati kluc hodiny do self.__layout (cas), ktora aktualne bezi. Ak taka hodina nebezi, vrati None."""
-        return self.__lessons_in_progress[day_index][column_index]
-
     def get_days(self):
         """Returns list of days in the week."""
         return self.__DAYS
 
-    # def get_columns_count(self) -> Dict:
-    #     """Vrati dict, kde klucom su dni v tyzdni a hodnoty su pocty stlpcov v danych dnoch."""
-    #     result = {}
-    #     for i in range(5):
-    #         result[self.__DAYS[i]] = len(self.__layout[i])
-    #     return result
 
     ########################### Class methods: ###########################
     @classmethod
