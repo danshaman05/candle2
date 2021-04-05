291050c61019e225487f13339f0a6d0cb36a19bc
Daniel Grohol <danshaman05@gmail.com>
2021-04-05 23:29:59 +0200

    docstrings
    also request.args['term'] calls were replaced by request.args.get('term') because of convention

--- candle/search/routes.py
+++ candle/search/routes.py
@@ -9,7 +9,15 @@ search = Blueprint('search', __name__)
 
 @search.route('/get_data/teachers', methods=['GET'])
 def get_teachers_json():
-    query_string = request.args['term']         # TODO: request could be JSON
+    """Find all teachers by query string typed in the search bar and send them back in a JSON.
+
+    Function will get data from the URL parameter sended from the JQueryUI Autocomplete search bar and will respond
+    with the JSON that will contain a list of teachers.
+
+    Request URL format: /get_data/teachers?term=query, where query is a string typed in the search bar.
+    Respond JSON format: list, where every element is a dictionary {'id':teacher.slug, 'value':teacher.fullname}.
+    """
+    query_string = request.args.get('term')
     query_string = query_string.replace(" ", "%")
     query_string = query_string.replace(".", "%")
     query_string = "%{}%".format(query_string)
@@ -19,18 +27,27 @@ def get_teachers_json():
             Teacher.fullname_reversed.like(query_string))) \
         .order_by(Teacher.family_name) \
         .limit(50).all()
+
     array = []
-    # send the teacher's slug and fullname via JSON:
     for t in teachers:
         array.append({'id': t.slug, 'value': t.fullname})     # do not change key names ('id' and 'value')! (the jquery-ui autocomplete widget will not work)
     return jsonify(array)
 
 @search.route('/get_data/rooms', methods=['GET'])
 def get_rooms_json():
-    query_string = request.args['term']
+    """Find all rooms by query string typed in the search bar and send them back in a JSON.
+
+    Function will get data from the URL parameter sended from the JQueryUI Autocomplete search bar and will respond
+    with the JSON that will contain a list of rooms.
+
+    Request URL format: /get_data/rooms?term=query, where query is a string typed in the search bar.
+    Respond JSON format: list, where every element is a dictionary {'id':room.name, 'value':room.name}.
+    """
+    query_string = request.args.get('term')
     query_string = query_string.replace(" ", "%")
     query_string = "%{}%".format(query_string)
     rooms = Room.query.filter(Room.name.like(query_string)).limit(50).all()
+
     array = []
     for r in rooms:
         array.append({'id': r.name, 'value': r.name})
@@ -38,11 +55,22 @@ def get_rooms_json():
 
 @search.route('/get_data/groups', methods=['GET'])
 def get_groups_json():
-    query_string = request.args['term']
+    """Find all student groups by query string typed in the search bar and send them back in a JSON.
+
+    Function will get data from the URL parameter sended from the JQueryUI Autocomplete search bar and will respond
+    with the JSON that will contain a list of student groups.
+
+    Request URL format: /get_data/groups?term=query, where query is a string typed in the search bar.
+    Respond JSON format: list, where every element is a dictionary {'id':group.name, 'value':group.name}.
+    """
+    query_string = request.args.get('term')
     query_string = query_string.replace(" ", "%")
     query_string = "%{}%".format(query_string)
     groups = StudentGroup.query.filter(StudentGroup.name.like(query_string)).limit(50).all()
+
     array = []
     for g in groups:
         array.append({'id': g.name, 'value': g.name})
     return jsonify(array)
+
+
