37971cc52e898712ac3adaf5723641c667ea61be
mr.Shu <mr@shu.io>
2021-04-16 18:07:08 +0200

    add: Login via CoSign
    * Ensure the auth endpoints are covered by CoSign authentification.
    
    * Fixes #8
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- candle/auth/views.py
+++ candle/auth/views.py
@@ -1,16 +1,28 @@
-from flask import Blueprint, redirect, request, url_for
+from flask import Blueprint, redirect, request, url_for, flash, current_app
 
 from candle.models import User
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user
+from functools import wraps
 
 
 auth = Blueprint('auth', __name__)
 
 
+def require_remote_user(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not current_app.config['DEBUG']:
+            if request.environ.get('REMOTE_USER') is None:
+                flash('User not logged in', 'error')
+                return redirect(url_for('timetable.home'))
+        return func(*args, **kwargs)
+    return wrapper
+
 
 @auth.route('/prihlasit', methods=['GET'])
+@require_remote_user
 def login():
-    ais_login = 'grohol2'  # TODO treba implementovat CoSign prihlasenie cez AIS ('grohol2' je moj AIS login)
+    ais_login = request.environ.get('REMOTE_USER')
     user = User.query.filter_by(login=ais_login).first()
     if user:
         login_user(user, remember=True)  # defaultne si zapamatame usera, aby sa najblizsie nemusel zas prihlasovat
@@ -27,6 +39,7 @@ def login():
 
 
 @auth.route('/odhlasit', methods=['GET'])
+@require_remote_user
 def logout():
     logout_user()
     # return redirect(request.referrer)
