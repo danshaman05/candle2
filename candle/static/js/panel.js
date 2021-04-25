c10d8174c81863ebd3c1297aeece2fb24bebd926
Daniel Grohol <danshaman05@gmail.com>
2021-04-25 19:31:01 +0200

    collapsible search panel

---
+++ candle/static/js/panel.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var buttons = document.getElementsByClassName("panel_cast_button");
+let i;
+for (i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+    this.classList.toggle("active");  // add or remove class
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "%";
+    }
+  });
+}
