dff724946c337e80274da4f5bfdbc06b9a19fade
Daniel Grohol <danshaman05@gmail.com>
2021-07-04 09:18:37 +0200

    Docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+# _url_candle= "http://danshaman.eu.pythonanywhere.com"
 _url_candle = "https://candle.fmph.uniba.sk"
 _url_localhost = "http://127.0.0.1:5000"
 _path_2016zima = "/2016-2017-zima"  # remove or edit if testing with newer Candle instance
@@ -7,13 +8,13 @@ _path_2016zima = "/2016-2017-zima"  # remove or edit if testing with newer Candl
 
 @pytest.fixture()
 def url_candle_2016():
-    """Unnecessary unless you test with 2016 Candle instance."""
+    """Needed only for tests with 2016 Candle instance."""
     return _url_candle + _path_2016zima
 
 
 @pytest.fixture()
 def url_localhost_2016():
-    """Unnecessary unless you test with 2016 Candle instance."""
+    """Needed only for tests with 2016 Candle instance."""
     return _url_localhost + _path_2016zima
 
 
